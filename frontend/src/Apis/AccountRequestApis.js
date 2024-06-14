2a2f22ff92f4a83b7605f40eca644186d237d735
muhammed rifkhan <rifkhanmuhammed17@gmail.com>
2024-06-14 12:15:11 +0530

    node change-2

--- frontend/src/Apis/AccountRequestApis.js
+++ frontend/src/Apis/AccountRequestApis.js
@@ -2,13 +2,9 @@ import axios from 'axios'
 
 const API = axios.create({
 	baseURL:
-		process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '',
+		process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : '',
 	withCredentials: true // Include credentials (cookies)
 })
-
-// const API = axios.create({ baseURL: 'https://account-back-4krv.onrender.com' })
-
-// export const getUser = (userId) => API.get(`user/${userId}`);
 
 export const createAccountRequest = formData =>
 	API.post('/accountRequest', formData)
--- frontend/src/Apis/RequestApis.js
+++ frontend/src/Apis/RequestApis.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const API = axios.create({
 	baseURL:
-		process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : '',
+		process.env.NODE_ENV === 'development' ? 'http://localhost:8000' : '',
 	withCredentials: true // Include credentials (cookies)
 })
 // const API = axios.create({ baseURL: 'https://account-back-4krv.onrender.com' })
